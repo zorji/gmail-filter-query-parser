e14d810d004c4d6083a184c09fb2e250ceb24bae
George Zhao <george.zhao@siteminder.com>
2020-07-05 13:36:11 +1000

    compile

--- src/grammar.ne
+++ src/grammar.ne
@@ -3,22 +3,58 @@
 # Inspired by https://github.com/justinkenel/js-sql-parse/blob/master/sql.ne
 # Operator precedence learnt from http://tobyho.com/video/How-to-Build-a-Parser-with-Nearley.js-Part-5-Operator-Precedence.html
 
+@{%
+const orExpr = (left, right) => {
+  return {
+    $or: [
+      ...unwrapIfOr(left),
+      ...unwrapIfOr(right),
+    ]
+  }
+}
+
+const andExpr = (left, right) => {
+  return {
+    $and: [
+      ...unwrapIfAnd(left),
+      ...unwrapIfAnd(right),
+    ]
+  }
+}
+
+// unwraps (A AND (B AND C)) to (A AND B AND C)
+const unwrapIfAnd = (expr) => {
+  if (expr.$and) {
+    return expr.$and.map(unwrapIfAnd).flat()
+  }
+  return [ expr ]
+}
+
+// unwraps (A OR (B OR C)) to (A OR B OR C)
+const unwrapIfOr = (expr) => {
+  if (expr.$or) {
+    return expr.$or.map(unwrapIfOr).flat()
+  }
+  return [ expr ]
+}
+%}
+
 expression -> OrExpr {% id %}
 
 OR -> "OR"#i
 AND -> "AND"#i
 
 OrExpr ->
-  AndExpr __ OR __ OrExpr {% d => ({ $or: [d[0], d[4]] }) %}
+  AndExpr __ OR __ OrExpr {% d => orExpr(d[0], d[4]) %}
   | AndExpr {% id %}
 
 AndExpr ->
-  Clause __ AND __ AndExpr {% d => ({ $and: [d[0], d[4]] }) %}
+  Clause __ AND __ AndExpr {% d => andExpr(d[0], d[4]) %}
   | ParenthesesExpr {% id %}
 
 ParenthesesExpr ->
-  "(" _ AndExpr __ OR __ OrExpr _ ")" {% d => ({ $or: [d[2], d[6]] }) %}
-  | "(" _ AndExpr __ AND __ OrExpr _ ")" {% d => ({ $and: [d[2], d[6]] }) %}
+  "(" _ AndExpr __ OR __ OrExpr _ ")" {% d => orExpr(d[2], d[6]) %}
+  | "(" _ AndExpr __ AND __ OrExpr _ ")" {% d => andExpr(d[2], d[6]) %}
   | Clause {% id %}
 
 Clause ->
--- src/parser.spec.ts
+++ src/parser.spec.ts
@@ -2,7 +2,7 @@ import { And, From, HasAttachment, Or, parse, QueryNode, serialise, Subject } fr
 
 describe('parser', () => {
 
-  it.only('should generate query correctly', () => {
+  it('should generate query correctly', () => {
 
     const node = Or(
       And(From('account@strata1.com'), HasAttachment()),
@@ -22,8 +22,6 @@ describe('parser', () => {
       And(From('donotreply@reates.govt'), HasAttachment()),
     )
 
-    console.log(JSON.stringify(node))
-
     expect(serialise(node))
       .toEqual('((from:(account@strata1.com) AND has:attachment) OR (from:(account@strata2.com) AND has:attachment) OR ((from:(noreply@strata3.com) OR from:(account@strata3.com)) AND subject:(Levy) AND has:attachment) OR (from:(noreply@internet.co) AND subject:(Invoice)) OR (from:(donotreply@reates.govt) AND has:attachment))')
 
@@ -39,8 +37,24 @@ describe('parser', () => {
         ],
       } as QueryNode)
 
-    expect(JSON.stringify(parse('((from:(account@strata1.com) AND has:attachment) OR (from:(account@strata2.com) AND has:attachment) OR ((from:(noreply@strata3.com) OR from:(account@strata3.com)) AND subject:(Levy) AND has:attachment) OR (from:(noreply@internet.co) AND subject:(Invoice)) OR (from:(donotreply@reates.govt) AND has:attachment))')))
-      .toEqual('')
+    expect(parse('((from:(account@strata1.com) AND has:attachment) OR (from:(account@strata2.com) AND has:attachment) OR ((from:(noreply@strata3.com) OR from:(account@strata3.com)) AND subject:(Levy) AND has:attachment) OR (from:(noreply@internet.co) AND subject:(Invoice)) OR (from:(donotreply@reates.govt) AND has:attachment))'))
+      .toEqual(Or(
+        And(From('account@strata1.com'), HasAttachment()),
+        And(From('account@strata2.com'), HasAttachment()),
+        And(
+          Or(
+            From('noreply@strata3.com'),
+            From('account@strata3.com'),
+          ),
+          Subject('Levy'),
+          HasAttachment(),
+        ),
+        And(
+          From('noreply@internet.co'),
+          Subject('Invoice'),
+        ),
+        And(From('donotreply@reates.govt'), HasAttachment()),
+      ))
 
   })
 
